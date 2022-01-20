898210c7eacfa1bdceb85a92fd7f8e47c7d41368
I Gusti Kade S <sugiantara.igk@gmail.com>
2022-01-20 20:07:21 +0700

    custom http response

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Response;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Response::macro('success', function ($data = null) {
+            return response()->json([
+                'success' => true,
+                'data' => $data
+            ]);
+        });
     }
 }
